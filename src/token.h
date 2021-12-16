bccda4b49542729e77856989ffcfad73a980e192
Artem Tarasenko <shabashab.04@gmail.com>
2021-12-16 19:40:05 +0200

    Create an enumeration with all possible tokens

---
+++ src/token.h
@@ -0,0 +1,8 @@
+#ifndef TOKEN_H
+#define TOKEN_H
+
+typedef enum token_e {
+	TOKEN_PLUS, TOKEN_MINUS, TOKEN_PRINT, TOKEN_INC_DATA, TOKEN_DEC_DATA, TOKEN_INPUT, TOKEN_WHILE_START, TOKEN_WHILE_END
+} token;
+
+#endif
